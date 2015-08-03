b22a7e8abd86016293c8ebba73ccecb815f54b0e
吴雄杰 <xiongjiewu@anjuke.com>
2015-08-03 23:20:44 +0800

    init login and home controller.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        echo 'oh yes,you done,this is home!';
+    }
+}
---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Controllers;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        echo 111;
+    }
+}
