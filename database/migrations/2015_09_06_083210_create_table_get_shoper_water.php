7212854e9413905647410d026b10eecf1bc990ba
朱晨聪 <chencongzhu@angejia.com>
2015-09-06 16:41:29 +0800

    增加用户领取亲水包的接口

---
+++ database/migrations/2015_09_06_083210_create_table_get_shoper_water.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableGetShoperWater extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('get_shop_water_log', function ($table) {
+            $table->integer('user_id')->unique()->default('0')->comment('商户ID');
+            $table->decimal('water_count')->default('0')->comment('亲水值');
+            $table->integer('giving_user_id')->unique()->default('0')->comment('领取的用户ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('get_shop_water_log');
+    }
+}
