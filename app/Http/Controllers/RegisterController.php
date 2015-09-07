b6b85624085a05a03852f8198ff3fa9261edb50b
朱晨聪 <chencongzhu@angejia.com>
2015-09-07 21:39:15 +0800

    返回错误

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -78,7 +78,7 @@ class RegisterController extends Controller
         }
         return [
             'status' => 'error',
-            'message' => $register['msg'],
+            'message' => $register['message'],
             'userInfo' => [],
         ];
     }
