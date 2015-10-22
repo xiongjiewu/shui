225882a1509f3d107764435004c724f1ef7e09b9
朱晨聪 <chencongzhu@angejia.com>
2015-10-22 21:54:09 +0800

    支持文章

--- database/migrations/2015_10_22_133833_create_table_user_support.php
+++ database/migrations/2015_10_22_133833_create_table_user_support.php
@@ -13,11 +13,10 @@ class CreateTableUserSupport extends Migration
     public function up()
     {
         Schema::create('user_support', function (Blueprint $table) {
-            $table->increments('id')->comment('自增ID');
             $table->integer('user_id')->default('0')->comment('用户ID');
-            $table->string('activity_id')->default('0')->comment('文章ID');
+            $table->integer('activity_id')->default('0')->comment('文章ID');
             $table->timestamps();
-            $table->index(['user_id', 'activity_id'], 'u_a');
+            $table->index('user_id');
         });
     }
 
